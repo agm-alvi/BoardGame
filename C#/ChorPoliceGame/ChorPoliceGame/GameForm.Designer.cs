cfcd37095a285bab1e4442347484455bb0dc7242
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2024-01-07 05:58:21 +0600

    C# winform project added and form created
    Initial Upload

---
+++ C#/ChorPoliceGame/ChorPoliceGame/GameForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace ChorPoliceGame
+{
+    partial class GameForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chit1 = new System.Windows.Forms.Button();
+            this.chit2 = new System.Windows.Forms.Button();
+            this.chit4 = new System.Windows.Forms.Button();
+            this.chit3 = new System.Windows.Forms.Button();
+            this.chitCHoosingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chit1
+            // 
+            this.chit1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.chit1.Location = new System.Drawing.Point(9, 75);
+            this.chit1.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.chit1.Name = "chit1";
+            this.chit1.Size = new System.Drawing.Size(100, 100);
+            this.chit1.TabIndex = 0;
+            this.chit1.Text = "1";
+            this.chit1.UseVisualStyleBackColor = true;
+            // 
+            // chit2
+            // 
+            this.chit2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.chit2.Location = new System.Drawing.Point(115, 75);
+            this.chit2.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.chit2.Name = "chit2";
+            this.chit2.Size = new System.Drawing.Size(100, 100);
+            this.chit2.TabIndex = 1;
+            this.chit2.Text = "2";
+            this.chit2.UseVisualStyleBackColor = true;
+            // 
+            // chit4
+            // 
+            this.chit4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.chit4.Location = new System.Drawing.Point(328, 75);
+            this.chit4.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.chit4.Name = "chit4";
+            this.chit4.Size = new System.Drawing.Size(100, 100);
+            this.chit4.TabIndex = 3;
+            this.chit4.Text = "4";
+            this.chit4.UseVisualStyleBackColor = true;
+            // 
+            // chit3
+            // 
+            this.chit3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.chit3.Location = new System.Drawing.Point(222, 75);
+            this.chit3.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.chit3.Name = "chit3";
+            this.chit3.Size = new System.Drawing.Size(100, 100);
+            this.chit3.TabIndex = 2;
+            this.chit3.Text = "3";
+            this.chit3.UseVisualStyleBackColor = true;
+            // 
+            // chitCHoosingLabel
+            // 
+            this.chitCHoosingLabel.AutoSize = true;
+            this.chitCHoosingLabel.Location = new System.Drawing.Point(36, 25);
+            this.chitCHoosingLabel.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
+            this.chitCHoosingLabel.Name = "chitCHoosingLabel";
+            this.chitCHoosingLabel.Size = new System.Drawing.Size(112, 37);
+            this.chitCHoosingLabel.TabIndex = 4;
+            this.chitCHoosingLabel.Text = "label1";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.chitCHoosingLabel);
+            this.Controls.Add(this.chit4);
+            this.Controls.Add(this.chit3);
+            this.Controls.Add(this.chit2);
+            this.Controls.Add(this.chit1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.Name = "GameForm";
+            this.Size = new System.Drawing.Size(450, 350);
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chit1;
+        private System.Windows.Forms.Button chit2;
+        private System.Windows.Forms.Button chit4;
+        private System.Windows.Forms.Button chit3;
+        private System.Windows.Forms.Label chitCHoosingLabel;
+    }
+}
---
+++ C#/ChorPoliceGame/ChorPoliceGame/HomePage.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace ChorPoliceGame
+{
+    partial class HomePage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.player1Name = new System.Windows.Forms.TextBox();
+            this.player4Name = new System.Windows.Forms.TextBox();
+            this.player3Name = new System.Windows.Forms.TextBox();
+            this.player2Name = new System.Windows.Forms.TextBox();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // player1Name
+            // 
+            this.player1Name.Location = new System.Drawing.Point(79, 70);
+            this.player1Name.Name = "player1Name";
+            this.player1Name.Size = new System.Drawing.Size(150, 23);
+            this.player1Name.TabIndex = 1;
+            // 
+            // player4Name
+            // 
+            this.player4Name.Location = new System.Drawing.Point(79, 157);
+            this.player4Name.Name = "player4Name";
+            this.player4Name.Size = new System.Drawing.Size(150, 23);
+            this.player4Name.TabIndex = 4;
+            // 
+            // player3Name
+            // 
+            this.player3Name.Location = new System.Drawing.Point(79, 128);
+            this.player3Name.Name = "player3Name";
+            this.player3Name.Size = new System.Drawing.Size(150, 23);
+            this.player3Name.TabIndex = 3;
+            // 
+            // player2Name
+            // 
+            this.player2Name.Location = new System.Drawing.Point(79, 99);
+            this.player2Name.Name = "player2Name";
+            this.player2Name.Size = new System.Drawing.Size(150, 23);
+            this.player2Name.TabIndex = 2;
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStartGame.Location = new System.Drawing.Point(45, 208);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(200, 80);
+            this.btnStartGame.TabIndex = 5;
+            this.btnStartGame.Text = "Start";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.btnStartGame);
+            this.Controls.Add(this.player2Name);
+            this.Controls.Add(this.player3Name);
+            this.Controls.Add(this.player4Name);
+            this.Controls.Add(this.player1Name);
+            this.Name = "HomePage";
+            this.Size = new System.Drawing.Size(450, 350);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox player1Name;
+        private System.Windows.Forms.TextBox player4Name;
+        private System.Windows.Forms.TextBox player3Name;
+        private System.Windows.Forms.TextBox player2Name;
+        private System.Windows.Forms.Button btnStartGame;
+    }
+}
